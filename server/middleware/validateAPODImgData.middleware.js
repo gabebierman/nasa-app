b1f32ee237deaa53f9d3cc76bb9ada47e298f6a8
gabebierman <gabebierman@gmail.com>
2022-11-15 16:38:30 -0600

    create middleware. Will require fixes while implementing

--- server/middleware/validateAPODImgData.middleware.js
+++ server/middleware/validateAPODImgData.middleware.js
@@ -1,6 +1,6 @@
 /**
- * pulls out gif information, checks for valid information and that the url is valid
- * sends an error message with the appropriate text if the object doesn't fot needed criteria
+ * pulls out img information, checks for valid information and that the url is valid
+ * sends an error message with the appropriate text if the object doesn't fit needed criteria
  *
  * @param {Request} req - Express request object
  * @param {Response} res - Express response object
@@ -12,14 +12,14 @@ export default function validateGifData(req, res, next) {
     const urlRegex =
         /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)(.gif)/;
 
-    const { id, date, link } = req.body;
-    if (!url || !url.toString().match(urlRegex) || url.length > 164) {
+    const { explanation, title, link, date, id } = req.body;
+    if (!link || !link.toString().match(urlRegex) || link.length > 164) {
         return res.send({ success: false, error: "Invalid URL" });
     }
 
-    if (!title || !date) {
+    if (!explanation || !title || !link || !date || !id) {
         return res.send({ success: false, error: "Invalid Data Provided" });
     }
-    req.body = { url, title, gif_id };
+    req.body = { explanation, title, link, date, id };
     return next();
 }
--- server/middleware/validateUserData.middleware.js
+++ server/middleware/validateUserData.middleware.js
@@ -1,10 +1,10 @@
 export default function validateData(req, res, next) {
-    const { username, password } = req.body;
-    if (!username || username.length < 4 || username.length > 20) {
-        return res.send({ error: "Username does not meet requirements", success: false });
-    }
-    if (!password || password.length < 8 || password.length > 20) {
-        return res.send({ error: "Password does not meet requirements", success: false });
+    const { userID } = req.body;
+    if (!userID) {
+        return res.send({
+            error: "UserID does not meet requirements. Please try signing in again",
+            success: false,
+        });
     }
     return next();
 }
