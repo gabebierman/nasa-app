1384ebba251cddc1afddbe18d6cab1fe8dab2857
gabebierman <gabebierman@gmail.com>
2022-11-17 10:03:09 -0600

    add babelrc and reinstall missing node packages

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 import express from "express";
-import userRoutes from "./server/routes/user.routes.js";
-import favoritesRoutes from "./server/routes/favorites.routes.js";
-// import passport from "./server/config/passport.config.js";
+import userRoutes from "./server/routes/user.routes";
+import favoritesRoutes from "./server/routes/favorites.routes";
+// import passport from "./server/config/passport.config";
 // import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/build"));
 app.use(express.json());
-app.use(cookieParser());
-app.use(passport.initialize());
+// app.use(cookieParser());
+// app.use(passport.initialize());
 app.use("/api/users", userRoutes);
 app.use("/api/favorites", favoritesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.enable("trust proxy");
+    app.use((req, res, next) => {
+        req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+    });
+}
+
 app.get("*", (req, res) => {
     return res.sendFile(__dirname + "/build/index.html");
 });
